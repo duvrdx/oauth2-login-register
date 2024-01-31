4748adccc9342db13876786649dfb8d26f50fa81
duvrdx <duvrdx@gmail.com>
2024-01-31 18:07:01 -0300

    fix: changing script

--- scripts/determine-version.js
+++ scripts/determine-version.js
@@ -1,11 +1,21 @@
 const { execSync } = require('child_process');
 
-const commitMessages = execSync('git log --pretty=format:%s HEAD ^$(git describe --tags --abbrev=0)').toString();
+try {
+  // Buscar a última tag
+  const latestTag = execSync('git describe --tags --abbrev=0 HEAD').toString().trim();
 
-if (commitMessages.includes('feat:')) {
-  console.log('minor');
-} else if (commitMessages.includes('fix:')) {
-  console.log('patch');
-} else {
-  console.log('patch');  // Pode ajustar conforme necessário
+  // Obter mensagens de commit desde a última tag
+  const commitMessages = execSync(`git log --pretty=format:%s ${latestTag}..HEAD`).toString();
+
+  // Determinar o tipo de versão com base nas mensagens de commit
+  if (commitMessages.includes('feat:')) {
+    console.log('minor');
+  } else if (commitMessages.includes('fix:')) {
+    console.log('patch');
+  } else {
+    console.log('patch');  // Pode ajustar conforme necessário
+  }
+} catch (error) {
+  console.error('Erro ao determinar a versão:', error.message);
+  process.exit(1);
 }
